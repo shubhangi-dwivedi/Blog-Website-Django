6463311c0e81f2f1e7109f4b148aa911ca16c563
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2025-03-16 15:40:41 +0530

    final commit

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.test),
-path('', views.login, name='login-page'),
-path('signup/', views.signup, name='signup-page'),
-path('home/', views.home, name='home-page'),
+    path('', views.signup, name='signup-page'),
+    path('login/', views.loginn, name='login-page'),
+    path('home/', views.home, name='home-page'),
+    path('newpost/', views.newPost, name='new-post'),
+    path('mypost/', views.myPost, name='my-post'),
+    path('signout/', views.signout, name='signout-btn'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,12 +1,16 @@
+from django.db.models.signals import post_save
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
+from .models import Posts
+from django.contrib.auth.models import User
+from . import models
 
 # Create your views here.
 def test(request):
     return render(request, 'blog/base.html')
 
-def login(request):
+def loginn(request):
     if request.method == 'POST':
         name = request.POST.get('uname')
         password = request.POST.get('upassword')
@@ -20,7 +24,40 @@ def login(request):
     return render(request, 'blog/login.html')
 
 def signup(request):
+    if request.method == 'POST':
+        name = request.POST.get('uname')
+        email= request.POST.get('uemail')
+        password = request.POST.get('upassword')
+        newUser = User.objects.create_user(username=name, email=email, password=password)
+        newUser.save()
+        return redirect('/login')
     return render(request, 'blog/signup.html')
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context ={
+        'posts' :Posts.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def newPost(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        npost = models.Posts(title=title, content=content, author=request.user)
+        npost.save()
+        return redirect('/home')
+
+    return render(request, 'blog/newpost.html')
+
+
+def myPost(request):
+    context = {
+        'posts': Posts.objects.filter(author=request.user)
+    }
+    return render(request, 'blog/mypost.html', context)
+
+
+def signout(request):
+    logout(request)
+    return redirect('/login')
